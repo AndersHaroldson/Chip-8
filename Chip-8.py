5f1e66a3180e61bd9f54335a9936dfd031b12156
AndersHaroldson <57378494+AndersHaroldson@users.noreply.github.com>
2024-01-19 17:53:14 -0600

    Update Chip-8.py

--- Chip-8.py
+++ Chip-8.py
@@ -4,26 +4,6 @@ import sys
 import time
 
 class Chip8():
-    keyset = {
-        pygame.K_1: 1,
-        pygame.K_2: 2,
-        pygame.K_3: 3,
-        pygame.K_4: 12,
-        pygame.K_q: 4,
-        pygame.K_w: 5,
-        pygame.K_e: 6,
-        pygame.K_r: 13,
-        pygame.K_a: 7,
-        pygame.K_s: 8,
-        pygame.K_d: 9,
-        pygame.K_f: 14,
-        pygame.K_z: 10,
-        pygame.K_x: 0,
-        pygame.K_b: 11,
-        pygame.K_v: 15
-
-    }
-
     def __init__(self):
         # 4 KB of memory
         self.memory = [0] * 4096
@@ -488,7 +468,27 @@ class Chip8():
                     pygame.draw.rect(self.screen, black, (x*10,y*10,10,10), 0)
 
         pygame.display.update()
+    
+    keyset = {
+        pygame.K_1: 1,
+        pygame.K_2: 2,
+        pygame.K_3: 3,
+        pygame.K_4: 12,
+        pygame.K_q: 4,
+        pygame.K_w: 5,
+        pygame.K_e: 6,
+        pygame.K_r: 13,
+        pygame.K_a: 7,
+        pygame.K_s: 8,
+        pygame.K_d: 9,
+        pygame.K_f: 14,
+        pygame.K_z: 10,
+        pygame.K_x: 0,
+        pygame.K_b: 11,
+        pygame.K_v: 15
 
+    }
+    
     def main(self):
         self.loadFontsIntoMemory()
         self.loadROM("Insert ROM path here")
